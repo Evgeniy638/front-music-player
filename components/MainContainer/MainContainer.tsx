565b74b96be7158fbe33839522d787139040cf55
Evgeniy <56478221+Evgeniy638@users.noreply.github.com>
2021-07-01 16:31:35 +0300

    added grid

--- components/MainContainer/MainContainer.tsx
+++ components/MainContainer/MainContainer.tsx
@@ -3,6 +3,8 @@ import MusicPlayerHead from "../MusicPlayerHead/MusicPlayerHead";
 import getConfig from 'next/config';
 import { setBaseUrl } from "../../api";
 
+import style from "./MainContainer.module.scss";
+
 setBaseUrl(getConfig().publicRuntimeConfig.BASE_URL);
 
 interface IPropsMainContainer {
@@ -13,7 +15,17 @@ const MainContainer= (props: IPropsMainContainer) => {
     return (
         <>
             <MusicPlayerHead/>
-            {props.children}
+            <div className={style.MainContainer}>
+                <nav className={style.MainContainer__nav}>
+
+                </nav>
+                <main className={style.MainContainer__main}>
+                    {props.children}
+                </main>
+                <footer className={style.MainContainer__footer}>
+
+                </footer>
+            </div>
         </>
     );
 }
